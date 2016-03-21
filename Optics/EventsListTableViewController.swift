04f65c212f42e73bb63812555ebfa45f8ec97be7
SwithFr <contact@swith.fr>
2016-03-21 22:15:49 +0100

    add actions for add button

--- Optics/EventsListTableViewController.swift
+++ Optics/EventsListTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class EventsListTableViewController: UITableViewController {
 
+    @IBOutlet var eventIdField: UITextField?
     var events:[JSON] = []
     let ModelEvent    = Event()
     
@@ -37,6 +38,10 @@ class EventsListTableViewController: UITableViewController {
         return .LightContent
     }
     
+    override func prefersStatusBarHidden() -> Bool {
+        return false
+    }
+    
     private func _setAndReloadData(data: NSData)
     {
         let events = JSON( data: data )
@@ -51,14 +56,81 @@ class EventsListTableViewController: UITableViewController {
         self.events.append( events["data"] )
         self.tableView.reloadData()
     }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        return false
-    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return events.count
+    }
+    
+    @IBAction func addEventBtnTapped(sender: AnyObject)
+    {
+        let popView = UIAlertController(
+            title: "Ajouter un évènement",
+            message: "Voulez vous créer un évènement ou en rejoindre un éxistant ?",
+            preferredStyle: .Alert
+        )
+        
+        let defaultAction = UIAlertAction( title: "Créer", style: .Default) {
+            ( alert: UIAlertAction! ) in
+            self._displayAddScreen()
+        }
+        
+        let otherAction = UIAlertAction( title: "Rejoindre", style: .Default) {
+            ( alert: UIAlertAction! ) in
+            self._displayGetScreen()
+        }
+        
+        let cancelAction = UIAlertAction( title: "Annuler", style: .Destructive, handler: nil )
+        
+        popView.addAction(defaultAction)
+        popView.addAction(otherAction)
+        popView.addAction(cancelAction)
+        
+        self.presentViewController( popView, animated: true, completion: nil )
+
+    }
+    
+    /*
+    *   Show the add event screen
+    */
+    private func _displayAddScreen() {
+        let addEventVC = self.storyboard?.instantiateViewControllerWithIdentifier( "addEventView" )
+        self.navigationController?.pushViewController( addEventVC!, animated: true )
+    }
+    
+    /*
+    *   Add input in UIAlertView
+    */
+    private func _addTextField( textField: UITextField! ){
+        textField.placeholder = "85171249"
+        self.eventIdField = textField
+    }
+    
+    /*
+    *   Show the event join alert
+    */
+    private func _displayGetScreen() {
+        
+        let popView = UIAlertController( title: "Rejoindre un évènement", message: "Saisissez le code de l'évènement", preferredStyle: .Alert )
+        
+        let defaultAction = UIAlertAction( title: "Rejoindre", style: .Default) {
+            ( alert: UIAlertAction! ) in
+            let iEventID:NSString = ( self.eventIdField?.text )!
+            
+            self.ModelEvent.join( iEventID as String ) {
+                data in
+                dispatch {
+                    self._addAndReloadData( data )
+                }
+            }
+        }
+        let cancelAction = UIAlertAction( title: "Annuler", style: .Destructive, handler: nil )
+        
+        popView.addTextFieldWithConfigurationHandler(_addTextField)
+        popView.addAction(defaultAction)
+        popView.addAction(cancelAction)
+        
+        self.presentViewController( popView, animated: true, completion: nil )
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
