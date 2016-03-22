58aa3839153ba56b9e8003a823c8d75a0cfad8d6
SwithFr <contact@swith.fr>
2016-03-22 14:03:20 +0100

    start event details controller

--- Optics/EventDetailsTableViewController.swift
+++ Optics/EventDetailsTableViewController.swift
@@ -16,28 +16,42 @@ class EventDetailsTableViewController: UITableViewController, UINavigationContro
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    /*
+    LIIGHT STATUS BAR
+    */
+    override internal func preferredStatusBarStyle() -> UIStatusBarStyle
+    {
+        return .LightContent
+    }
+    
+    override func prefersStatusBarHidden() -> Bool
+    {
+        return false
+    }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return 0
     }
 
