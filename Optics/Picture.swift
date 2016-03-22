3d11f601e0709e2b776e42786483c76af2497e49
SwithFr <contact@swith.fr>
2016-03-22 21:43:58 +0100

    add Picture model

---
+++ Optics/Picture.swift
@@ -0,0 +1,45 @@
+//
+//  Picture.swift
+//  Optics
+//
+//  Created by Jérémy Smith on 22/03/2016.
+//  Copyright © 2016 Jérémy Smith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Picture: Model
+{
+    func getAllFromEventID(sEventID: String, completionHandler: (data: NSData) -> Void)
+    {
+        self.setParams( [ "eventid": sEventID ] )
+        self.get( "pictures" , authenticate: true) {
+            error, data in
+            dispatch_async( dispatch_get_main_queue() ) {
+                if error != nil {
+                    print("error")
+                }
+                
+                completionHandler( data: data )
+            }
+        }
+    }
+    
+    func uploadImg(sEventID: String, img: UIImage, completionHandler: (data: NSData) -> Void)
+    {
+        self.uploadImage( sEventID ,image: img ) {
+            error, data in
+            if error != nil {
+                print( "Error on upload" )
+                return
+            }
+            
+            dispatch_async( dispatch_get_main_queue() ) {
+                completionHandler( data: data )
+            }
+        }
+
+    }
+    
+}
