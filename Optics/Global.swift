f5bfa290e934f28eb8a9d848d7b4805a671be4fc
SwithFr <contact@swith.fr>
2016-03-30 22:35:48 +0200

    add a JSON method

--- Optics/Global.swift
+++ Optics/Global.swift
@@ -15,3 +15,26 @@ func dispatch(completion: () -> Void)
         completion()
     }
 }
+
+func JSONStringify(value: AnyObject,prettyPrinted:Bool = false) -> String{
+    
+    let options = prettyPrinted ? NSJSONWritingOptions.PrettyPrinted : NSJSONWritingOptions(rawValue: 0)
+    
+    
+    if NSJSONSerialization.isValidJSONObject(value) {
+        
+        do{
+            let data = try NSJSONSerialization.dataWithJSONObject(value, options: options)
+            if let string = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                return string as String
+            }
+        }catch {
+            
+            print("error")
+            //Access error here
+        }
+        
+    }
+    return ""
+    
+}
